15e7727aea886bfc0a588ad2b8cd220cb26c3d7c
Tri <trimpham98@gmail.com>
2023-07-13 23:39:22 -0700

    Use one line of code to add flight helmet to scene

--- src/script.js
+++ src/script.js
@@ -23,10 +23,12 @@ const scene = new THREE.Scene();
 const gltfLoader = new GLTFLoader();
 
 gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
-  const children = [...gltf.scene.children];
-  for (const child of children) {
-    scene.add(child);
-  }
+  // const children = [...gltf.scene.children];
+  // for (const child of children) {
+  //   scene.add(child);
+  // }
+
+  scene.add(gltf.scene);
 });
 
 /**
